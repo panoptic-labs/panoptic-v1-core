350797c1b9ef75f8636668e4960bee373285c41e
dyedm1 <henry@hyf.one>
2023-11-14 04:01:04 -0500

    fix: add swap bounding

--- test/foundry/core/PanopticPool.t.sol
+++ test/foundry/core/PanopticPool.t.sol
@@ -3576,7 +3576,11 @@ contract PanopticPoolTest is PositionUtils {
                     10_000 +
                     int128(tokensOwed0)
             ),
-            (uint256(int256(shortAmounts.rightSlot())) + tokensOwed0) / 1_000_000 + 10
+            (uint256(int256(shortAmounts.rightSlot())) + tokensOwed0) /
+                1_000_000 +
+                (expectedSwaps[0] + expectedSwaps[1]) /
+                100 +
+                10
         );
 
         assertApproxEqAbs(
