be62ced15cf010268ac0e7bdf1802e9cebba449b
Suthan Somadeva <106453938+PeakyCryptos@users.noreply.github.com>
2023-11-21 06:53:00 -0500

    feat: add dai/usdc 1 bps

--- test/foundry/core/CollateralTracker.t.sol
+++ test/foundry/core/CollateralTracker.t.sol
@@ -362,7 +362,9 @@ contract CollateralTrackerTest is Test, PositionUtils {
     IUniswapV3Pool constant MATIC_ETH_30 =
         IUniswapV3Pool(0x290A6a7460B308ee3F19023D2D00dE604bcf5B42);
 
-    IUniswapV3Pool[3] public pools = [USDC_WETH_5, WBTC_ETH_30, MATIC_ETH_30];
+    IUniswapV3Pool constant DAI_USDC_1 = IUniswapV3Pool(0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168);
+
+    IUniswapV3Pool[3] public pools = [USDC_WETH_5, WBTC_ETH_30, MATIC_ETH_30, DAI_USDC_1];
 
     // Mainnet factory address
     IUniswapV3Factory V3FACTORY = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
