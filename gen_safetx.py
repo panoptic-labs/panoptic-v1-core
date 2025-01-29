c5088bd21d373c5f6569a05f7985082ddaf1a443
dyedm1 <henry@hyf.one>
2025-01-29 10:47:08 -0800

    chore: update scripts

--- gen_safetx.py
+++ gen_safetx.py
@@ -6,9 +6,43 @@ with open("deployment-info.json", "r") as file:
 
 os.makedirs('./safe-txns', exist_ok=True)
 
+deployBatches = []
+
+deployBatches.append({
+    "chainId": os.environ.get("CHAIN_ID") or "1",
+    "meta": {
+        "name": f"Deploy data contracts 0-3",
+    },
+    "transactions": []
+})
+
+deployBatches.append({
+    "chainId": os.environ.get("CHAIN_ID") or "1",
+    "meta": {
+        "name": f"Deploy data contracts 4-6 and logic contracts 0-3",
+    },
+    "transactions": []
+})
+
+deployBatches.append({
+    "chainId": os.environ.get("CHAIN_ID") or "1",
+    "meta": {
+        "name": f"Deploy logic contract 4",
+    },
+    "transactions": []
+})
+
+deployBatches.append({
+    "chainId": os.environ.get("CHAIN_ID") or "1",
+    "meta": {
+        "name": f"Deploy logic contracts 5-6",
+    },
+    "transactions": []
+})
+
 for idx, contract in enumerate(deploymentInfo["dataContracts"]):
     safeTx = {
-        "chainId": "1",
+        "chainId": os.environ.get("CHAIN_ID") or "1",
         "meta": {
             "name": f"Deploy data contract {idx} at {contract["address"]}",
         },
@@ -39,7 +73,7 @@ for idx, contract in enumerate(deploymentInfo["dataContracts"]):
                     "payable": False
                 },
                 "contractInputsValues": {
-                    "to": "0x82BF455e9ebd6a541EF10b683dE1edCaf05cE7A1",
+                    "to": contract["salt"][:42],
                     "id": str(int(contract["salt"], 16)),
                     "nonce": str(contract["nonce"])
                 }
@@ -65,7 +99,6 @@ for idx, contract in enumerate(deploymentInfo["dataContracts"]):
                     "payable": True
                 },
                 "contractInputsValues": {
-                    "to": "0x82BF455e9ebd6a541EF10b683dE1edCaf05cE7A1",
                     "id": str(int(contract["salt"], 16)),
                     "initcode": contract["initcode"]
                 }
@@ -73,12 +106,31 @@ for idx, contract in enumerate(deploymentInfo["dataContracts"]):
         ]
     }
 
+    if idx < 4: deployBatches[0]["transactions"] += safeTx["transactions"]
+    else: deployBatches[1]["transactions"] += safeTx["transactions"]
+
     with open(f"./safe-txns/dataDeploy_{idx}.json", "w") as output_file:
         json.dump(safeTx, output_file)
 
+libTxs = {
+    "chainId": os.environ.get("CHAIN_ID") or "1",
+    "meta": {
+        "name": f"Deploy all libraries",
+    },
+    "transactions": []
+}
+
+coreTxs = {
+    "chainId": os.environ.get("CHAIN_ID") or "1",
+    "meta": {
+        "name": f"Deploy all core contracts",
+    },
+    "transactions": []
+}
+
 for idx, contract in enumerate(deploymentInfo["logicContracts"]):
     safeTx = {
-        "chainId": "1",
+        "chainId": os.environ.get("CHAIN_ID") or "1",
         "meta": {
             "name": f"Deploy contract {contract["contractName"]} at {contract["address"]}",
         },
@@ -109,7 +161,7 @@ for idx, contract in enumerate(deploymentInfo["logicContracts"]):
                     "payable": False
                 },
                 "contractInputsValues": {
-                    "to": "0x82BF455e9ebd6a541EF10b683dE1edCaf05cE7A1",
+                    "to": contract["salt"][:42],
                     "id": str(int(contract["salt"], 16)),
                     "nonce": str(contract["nonce"])
                 }
@@ -135,7 +187,6 @@ for idx, contract in enumerate(deploymentInfo["logicContracts"]):
                     "payable": True
                 },
                 "contractInputsValues": {
-                    "to": "0x82BF455e9ebd6a541EF10b683dE1edCaf05cE7A1",
                     "id": str(int(contract["salt"], 16)),
                     "initcode": contract["initcode"]
                 }
@@ -143,5 +194,20 @@ for idx, contract in enumerate(deploymentInfo["logicContracts"]):
         ]
     }
 
+    if idx < 4: deployBatches[1]["transactions"] += safeTx["transactions"]
+    elif idx == 4: deployBatches[2]["transactions"] += safeTx["transactions"]
+    else: deployBatches[3]["transactions"] += safeTx["transactions"]
+
     with open(f"./safe-txns/deploy_{idx}_{contract["contractName"]}.json", "w") as output_file:
         json.dump(safeTx, output_file)
+
+
+for idx, batch in enumerate(deployBatches):
+    with open(f"./safe-txns/batch_deploy_{idx}.json", "w") as output_file:
+        json.dump(batch, output_file)
+
+with open("./safe-txns/deploy_all_1_libraries.json", "w") as output_file:
+    json.dump(libTxs, output_file)
+
+with open("./safe-txns/deploy_all_2_core.json", "w") as output_file:
+    json.dump(coreTxs, output_file)
